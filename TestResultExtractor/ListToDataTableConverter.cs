f780177b4ee6cfb7a8bbd3096e583028cd4e9f91
sooraj-sn <53735014+sooraj-sn@users.noreply.github.com>
2020-05-06 12:57:21 +0530

    Initial Working Version
    This is an initial working version where we get the test result list with success status, time and test name.

---
+++ TestResultExtractor/ListToDataTableConverter.cs
@@ -0,0 +1,40 @@
+﻿#region SystemNamespaces
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+#endregion
+
+namespace TestResultExtractor
+{
+    /// <summary>
+    /// ListToDataTableConverter
+    /// </summary>
+    public class ListToDataTableConverter
+    {
+        /// <summary>
+        /// Convert List to Data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        public DataTable ToDataTable<T>(List<T> items)
+        {
+            DataTable dataTable = new DataTable(typeof(T).Name);
+            PropertyInfo[] Props = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            foreach (PropertyInfo prop in Props)
+            {
+                dataTable.Columns.Add(prop.Name);
+            }
+            foreach (T item in items)
+            {
+                var values = new object[Props.Length];
+                for (int i = 0; i < Props.Length; i++)
+                {
+                    values[i] = Props[i].GetValue(item, null);
+                }
+                dataTable.Rows.Add(values);
+            }
+            return dataTable;
+        }
+    }
+}
---
+++ TestResultExtractor/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿#region SystemNamespaces
+using System;
+using System.Windows;
+#endregion
+
+#region Microsoft Win32 Namespaces
+using Microsoft.Win32;
+#endregion
+
+namespace TestResultExtractor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string xmlFileName;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Upload File Event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_upload_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = false;
+            fileDialog.Filter = "Nunit Xml Files|*.xml";
+            fileDialog.DefaultExt = ".xml";
+            Nullable<bool> dialogOK = fileDialog.ShowDialog();
+            if (dialogOK == true)
+            {
+                txt_fileupload.Text = fileDialog.FileName;
+                txt_fileupload.IsReadOnly = true;
+                xmlFileName = txt_fileupload.Text;
+                lbl_uploadlabel.Visibility = Visibility.Hidden;
+            }
+        }
+    
+        /// <summary>
+        /// Export to Excel Event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_export_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(xmlFileName))
+            {
+                lbl_uploadlabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lbl_uploadlabel.Visibility = Visibility.Hidden;
+                XmlToCSVEngine engine = new XmlToCSVEngine();
+                engine.DisplayResultsInExcel(engine.GetTestResultSet(xmlFileName));
+            }
+            
+
+        }
+
+        /// <summary>
+        /// View Results in Grid View Event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_vwresult_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(xmlFileName))
+            {
+                lbl_uploadlabel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                lbl_uploadlabel.Visibility = Visibility.Hidden;
+                XmlToCSVEngine engine = new XmlToCSVEngine();
+                var resultset = engine.GetTestResultSet(xmlFileName);
+                TestResultViewer tviewer = new TestResultViewer();
+                var dataTable = engine.GetTestResultsInDataTable(resultset);
+                tviewer.DisplayData(dataTable);
+                tviewer.Show();
+            }
+           
+        }
+    }
+}
